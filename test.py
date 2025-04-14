7532f375d54cc3532a1095254a2e2ba1af3b1746
annwhocodes <ananyeah98@gmail.com>
2025-04-14 23:21:31 +0530

    Removed Test Files

--- test.py
+++
@@ -1,9 +0,0 @@
-import requests
-api_key = "AIzaSyAdZf_dtGTSdU2DpYyHzWE4O8C9GkCeNyI"
-url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={api_key}"
-payload = {
-    "contents": [{"parts": [{"text": "Hello world"}]}]
-}
-response = requests.post(url, json=payload)
-print(response.status_code)
-print(response.json())
